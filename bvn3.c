164325b3231367d878ca86406c81cc2127f0a98e
vulananh29112007-art <vulananh29112007@gmail.com>
2025-11-19 00:39:45 +0700

    Update bvn3.c

--- bvn3.c
+++ bvn3.c
@@ -9,13 +9,13 @@ int main(){
 	struct Information sv[5];
 	for(int i = 0;i<5;i++){
 		printf("Nhap ten cua sinh vien %d: ",i+1);
-	    fgets(sv[i].name,sizeof(sv),stdin);
+	    fgets(sv[i].name,sizeof(sv[i].name),stdin);
 	    sv[i].name[strcspn(sv[i].name,"\n")] = '\0';
 	    printf("Nhap tuoi cua sinh vien %d: ",i+1);
 	    scanf("%d",&sv[i].age);
 	    getchar();
 	    printf("Nhap sdt cua sinh vien %d: ",i+1);
-	    fgets(sv[i].phoneNumber,sizeof(sv),stdin);
+	    fgets(sv[i].phoneNumber,sizeofsv[i].phoneNumber),stdin);
 	    sv[i].phoneNumber[strcspn(sv[i].phoneNumber,"\n")] = '\0';
 	}
 	
@@ -28,4 +28,5 @@ int main(){
 
    return 0;
 }
+
 
